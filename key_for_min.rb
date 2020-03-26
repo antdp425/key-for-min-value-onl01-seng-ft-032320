8b0b3cf24beb30edb054f867856057e17eb44ba4
Anthony DiPietrantonio <antdp425@aol.com>
2020-03-26 14:41:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -17,10 +17,21 @@ def key_for_min_value(name_hash)
   # end
   lowestKey = ""
   lowestValue = 0
+  count = 0
     if name_hash.length == 0
       nil
     elsif name_hash.length >= 1
-      name_hash.collect do |key,value|
-        #pop/push/unshift
-        #assing lowestK + lowetV
+      name_hash.each do |key,value|
+        count += 1
+        if count == 1
+          lowestKey = key
+          lowestValue = value
+        end
+        if count > 1 && name_hash[key] < lowestValue
+          lowestKey = key
+          lowestValue = value
+        end
+      end
+    end
+    lowestKey
 end
